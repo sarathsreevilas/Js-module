a6a5d43716caac5481bb310385672773851dd4f4
smug <supersarath007@gmail.com>
2022-08-26 19:23:50 +0530

    module

---
+++ modules.js
@@ -0,0 +1,8 @@
+import { myFunc as testFun, myString as str} from "./variables.js";
+
+import handleInputAsString from "./variables.js";
+
+console.log(testFun());
+console.log(str);
+
+console.log(handleInputAsString());
---
+++ variables.js
@@ -0,0 +1,15 @@
+const myString = "JavaScript";
+
+const myFunc = () => {
+  return 10;
+};
+
+export { myString, myFunc };
+
+function handleChange() {
+  return "Input Value";
+}
+
+export default handleChange;
+
+
